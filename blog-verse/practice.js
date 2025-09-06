33028f4ef674fa5ee06d42b45e6efc9c84f9bff3
NeelamSirisha <neelamjnananagasirisha2005@gmail.com>
2025-09-06 12:33:23 +0530

    commited

--- blog-verse/practice.js
+++ blog-verse/practice.js
@@ -1,1 +1,34 @@
 console.log("Hello Sirisha")
+
+let name = "DevAstra";
+console.log(name)
+
+let num = 36;
+console.log(num)
+
+let isLogged=false;
+console.log(isLogged)
+
+let X;
+console.log(X)
+
+let y=null;
+
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof X)
+
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"sirisha",
+    age:20
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
